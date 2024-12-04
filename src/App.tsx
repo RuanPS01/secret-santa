462eb3828e34efe5e7bc93b7b83be6fa3e7a2ce7
Ruan Patrick <ruanpatrick.s@hotmail.com>
2024-12-03 21:22:21 -0300

    ci: deploy configuration and adjustments for build

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { Setup } from './pages/Setup';
 import { Reveal } from './pages/Reveal';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/setup" element={<Setup />} />
         <Route path="/reveal" element={<Reveal />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/secret-santa/', // Adicione esta linha
   optimizeDeps: {
-    exclude: ["lucide-react"],
-  },
+    exclude: ['lucide-react']
+  }
 });
