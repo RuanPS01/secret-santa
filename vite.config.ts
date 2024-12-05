d4a7a1aa5cb02b80eff4bcb4c1fbbd48aba78d14
Ruan Patrick <ruanpatrick.s@hotmail.com>
2024-12-04 21:16:21 -0300

    docs: remove comments

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/secret-santa/', // Adicione esta linha
+  base: '/secret-santa/',
   optimizeDeps: {
     exclude: ['lucide-react']
   }
